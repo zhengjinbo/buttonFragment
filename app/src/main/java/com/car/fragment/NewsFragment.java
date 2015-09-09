abe20455519ae9ec62121082dcbee7a3b89b5fd0
zhengjinbo <785348547@qq.com>
2015-09-09 20:51:47 +0800

    Initial commit

---
+++ app/src/main/java/com/car/fragment/NewsFragment.java
@@ -0,0 +1,23 @@
+package com.car.fragment;
+
+import com.car.activity.R;
+
+/**
+ * 资讯
+ * 
+ * @author blue
+ */
+public class NewsFragment extends BaseFragment
+{
+	@Override
+	protected int getLayoutId()
+	{
+		return R.layout.fragment_news_main;
+	}
+
+	@Override
+	protected void initParams()
+	{
+	}
+
+}
